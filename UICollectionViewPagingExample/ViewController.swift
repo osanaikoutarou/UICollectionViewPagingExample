8d0e36832240bf972379c706f11f1ab3b419cc7c
osanaikoutarou <osanaikoutarou@gmail.cpm>
2021-08-21 09:53:53 +0900

    Initial Commit

---
+++ UICollectionViewPagingExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICollectionViewPagingExample
+//
+//  Created by 長内幸太郎 on 2021/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
