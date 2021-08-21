c8f01b3a1af0e6e518b410df4a5ebcfaba9bd2d6
osanaikoutarou <osanaikoutarou@gmail.cpm>
2021-08-21 16:55:58 +0900

    first commit

---
+++ UICollectionViewPagingExample/Object/SampleCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SampleCollectionViewCell.swift
+//  UICollectionViewPagingExample
+//
+//  Created by 長内幸太郎 on 2021/08/21.
+//
+
+import UIKit
+
+class SampleCollectionViewCell: UICollectionViewCell {
+    
+}
